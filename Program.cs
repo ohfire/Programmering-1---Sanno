2158215bd066ed27aab9d70e9685c44751376d6f
ohfire <143172200+ohfire@users.noreply.github.com>
2023-09-06 10:41:50 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Uppgift_1_1
+{
+class Program
+ {
+    static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1.");
+            Console.WriteLine("Du har lärt dig skriva ut tecken som Quote och \.");
+        }
+    }
+}
