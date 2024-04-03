069f2a26c6650e434435a9d6cb81d7a2970496c3
ohfire <143172200+ohfire@users.noreply.github.com>
2024-04-03 22:02:12 +0200

    Create program.cs

---
+++ Inlämningsuppgifter /OOP/övning1/program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uppgift_oop1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            bil p = new bil("volvo", "v70", "mörkblå", 13, 25000);
+
+            bil p2 = new bil("audi", "r8", "vit", 4, 5000);
+
+            p.Mymetod();
+            Console.WriteLine("");
+            p2.Mymetod();
+        }
+    }
+}
